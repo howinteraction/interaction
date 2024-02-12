abc04862291b01b1a7bab9285f8bfc16afa025fb
erv2bh <pagaous@gmail.com>
2024-02-12 15:15:01 +0900

    feat: proptypes 검사 로직 추가

--- src/components/StageClearModal/index.jsx
+++ src/components/StageClearModal/index.jsx
@@ -1,33 +1,34 @@
+import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const ModalContainer = styled.div`
+  display: flex;
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
   background: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
 `;
 
 const ModalContent = styled.div`
-  background: #fff;
   padding: 20px;
+  background: #fff;
   border-radius: 5px;
   text-align: center;
 `;
 
 const CloseButton = styled.button`
+  padding: 10px 20px;
+  margin-top: 10px;
   background: #007bff;
   color: #fff;
   border: none;
-  padding: 10px 20px;
   border-radius: 3px;
   cursor: pointer;
   font-size: 16px;
-  margin-top: 10px;
 `;
 
 export default function StageClearModal({ onClose }) {
@@ -46,4 +47,8 @@ export default function StageClearModal({ onClose }) {
       </ModalContent>
     </ModalContainer>
   );
+}
+
+StageClearModal.propTypes = {
+  onClose: PropTypes.func.isRequired,
 };
