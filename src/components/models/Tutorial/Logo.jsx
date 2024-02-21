501d00a767b8be28d9c345cc82f203f100569f3b
ORIGINCHOI <116258834+ORIGINCHOI@users.noreply.github.com>
2024-02-22 05:25:39 +0900

    fix: Logo 컴포넌트 회전값 추가

--- src/components/models/Tutorial/Logo.jsx
+++ src/components/models/Tutorial/Logo.jsx
@@ -1,4 +1,5 @@
-import { useLoader } from "@react-three/fiber";
+import { useRef } from "react";
+import { useFrame, useLoader } from "@react-three/fiber";
 import PropTypes from "prop-types";
 
 import * as THREE from "three";
@@ -8,9 +9,16 @@ export default function Logo({ position, scale, rotation }) {
     THREE.TextureLoader,
     "/assets/images/tutorial-picture/logo-removebg.png",
   );
+  const logoRef = useRef();
+
+  useFrame(() => {
+    logoRef.current.rotation.x += 0.005;
+    logoRef.current.rotation.y += 0.005;
+    logoRef.current.rotation.z += 0.005;
+  });
 
   return (
-    <mesh position={position} scale={scale} rotation={rotation}>
+    <mesh position={position} scale={scale} rotation={rotation} ref={logoRef}>
       <planeGeometry attach="geometry" />
       <meshStandardMaterial
         attach="material"
