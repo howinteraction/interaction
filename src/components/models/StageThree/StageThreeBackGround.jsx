4bace5afbb37eed64bdad54b18cb73de4e1f6324
seohag <sscgeum@gmail.com>
2024-02-21 14:59:24 +0900

    chore: 스테이지3 Background glb 파일명 수정

--- src/components/models/StageThree/StageThreeBackGround.jsx
+++ src/components/models/StageThree/StageThreeBackGround.jsx
@@ -1,7 +1,7 @@
 import { useGLTF } from "@react-three/drei";
 
 export default function StageThreeBackground() {
-  const { scene } = useGLTF("/assets/glb/stage3-ground8.glb");
+  const { scene } = useGLTF("/assets/glb/stage3-ground.glb");
 
   scene.traverse((child) => {
     child.receiveShadow = true;
@@ -11,4 +11,4 @@ export default function StageThreeBackground() {
   return <primitive object={scene} />;
 }
 
-useGLTF.preload("/assets/glb/stage3-ground8.glb");
+useGLTF.preload("/assets/glb/stage3-ground.glb");
