526df6dfa6317fca436fc1bcd4e084405c4ed50a
ORIGINCHOI <116258834+ORIGINCHOI@users.noreply.github.com>
2024-02-22 05:26:24 +0900

    refactor: CameraMotion 컴포넌트 PointerLock 관련 로직 삭제

--- src/components/CameraMotion/index.jsx
+++ src/components/CameraMotion/index.jsx
@@ -1,7 +1,5 @@
-import { useRef } from "react";
-import PropTypes from "prop-types";
-import { PointerLockControls } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
+import PropTypes from "prop-types";
 
 import * as THREE from "three";
 
@@ -10,23 +8,14 @@ export default function CameraMotion({
   lerpFactor,
   targetDirection,
 }) {
-  const { camera, gl } = useThree();
-  const controls = useRef();
+  const { camera } = useThree();
   const targetPositionVector = new THREE.Vector3(...targetPosition);
   const targetDirectionVector = new THREE.Vector3(...targetDirection);
 
-  if (controls.current && controls.current.isLocked) {
-    controls.current.unlock();
-  }
-
   useFrame(() => {
-    if (!controls.current.isLocked) {
-      camera.position.lerp(targetPositionVector, lerpFactor);
-      camera.lookAt(targetDirectionVector);
-    }
+    camera.position.lerp(targetPositionVector, lerpFactor);
+    camera.lookAt(targetDirectionVector);
   });
-
-  return <PointerLockControls ref={controls} args={[camera, gl.domElement]} />;
 }
 
 CameraMotion.propTypes = {
